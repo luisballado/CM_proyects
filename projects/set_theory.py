3ed93a2b87a8b2ccf29abf885874a96f534b20fb
Luis Alberto Ballado Aradias <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2022-10-11 20:46:12 -0500

    estructura clase

---
+++ projects/set_theory.py
@@ -0,0 +1,48 @@
+'''
+Clase para manejar las operaciones de teoria de conjuntos
+@autor: Luis Ballado
+'''
+
+class set_theory():
+
+    def __init__():
+       self.__author__ = "Luis Ballado"
+       self.__version__ = "1.0"
+       
+    def display():
+        return None
+
+    def cardinality():
+        return None
+
+    def power_set():
+        return None
+
+    def possible_partitions():
+        return None
+
+    def comparision():
+        return None
+
+    def subset():
+        return None
+
+    def proper_subset():
+        return None
+
+    def difference():
+        return None
+
+    def symmetric_diff():
+        return None
+
+    def union():
+        return None
+
+    def intersection():
+        return None
+
+    def cartesian_product():
+        return None
+       
+       
